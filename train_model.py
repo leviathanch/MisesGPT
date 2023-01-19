75e38ffb82f8716ad1450baa87710d7fc8e05ede
David Lanzendörfer <leviathan@libresilicon.com>
2023-01-19 10:24:07 +0000

    Simplify trainer

--- train_model.py
+++ train_model.py
@@ -13,81 +13,26 @@ from transformers import (
     GPT2Config,
     GPT2LMHeadModel,
     GPT2Tokenizer,
-    TextDataset,
     DataCollatorForLanguageModeling,
     Trainer,
     TrainingArguments,
   )
 
-from torch.utils.data import Dataset, random_split
-from misesgpt.html_fetcher import MisesHTMLBookCatalog
-from misesgpt.epub_fetcher import MisesEPUBookCatalog
+from torch.utils.data import random_split
+from misesgpt.dataset import MisesDataset
 
-print("Preparing tokenizer")
+max_length = 1024
 
-dscache = './dataset_cache/'
 tokenizer = GPT2Tokenizer.from_pretrained(
   'gpt2',
   bos_token = '<|startoftext|>',
   eos_token = '<|endoftext|>',
   pad_token = '<|pad|>'
 )
+dataset = MisesDataset(tokenizer, max_length)
 
-print("Preparing training data")
-
-def append_data(file, books):
-  maxlen = 0
-  for book in books:
-    for p in books[book]:
-      s = '<|startoftext|>' + p + '<|endoftext|>'
-      sl = len(tokenizer.encode(s))
-      if sl < 1024:
-        maxlen = sl if sl > maxlen else maxlen
-        file.write(s+'\n')
-  return maxlen
-
-# prepare training data:
-df = 'data.txt'
-lens = []
-if exists(df):
-  remove(df)
-with open(df,'a') as file:
-  lens.append(append_data(file, MisesHTMLBookCatalog().books_json))
-  lens.append(append_data(file, MisesEPUBookCatalog().books_json))
-  file.close()
-
-max_length = max(lens)
-assert(max_length > 0)
-block_size = pow(2, ceil(log(max_length)/log(2)));
-print("Maxlen is", max_length)
-print("Block size is", block_size)
-
-# ---------
-print("Reinitialize tokenizer")
-tokenizer = GPT2Tokenizer.from_pretrained(
-  'gpt2',
-  bos_token = '<|startoftext|>',
-  eos_token = '<|endoftext|>',
-  pad_token = '<|pad|>',
-  max_length = max_length,
-)
-# ---------
-
-print("Preparing data set")
-
-if not exists(dscache):
-  mkdir(dscache)
-
-dataset = TextDataset(
-  tokenizer = tokenizer,
-  file_path = './data.txt',
-  block_size = block_size,
-  cache_dir = dscache,
-)
 train_size = int(0.9 * len(dataset))
 train_dataset, val_dataset = random_split(dataset, [train_size, len(dataset) - train_size])
-
-print("Create/load model")
 
 config = GPT2Config(n_layer=6, n_head=6, n_embd=192)
 if exists('./base_model'):
@@ -108,6 +53,8 @@ print("Set up collator")
 data_collator = DataCollatorForLanguageModeling(
   tokenizer = tokenizer,
   mlm = False,
+  #mlm = True,
+  #mlm_probability = 0.15,
 )
 
 print("Run training")
