9205ecf748a09fff2246086755e2eaad4364c9d0
David Lanzendörfer <leviathan@libresilicon.com>
2023-01-19 11:36:44 +0000

    Cleanup

--- inference_test.py
+++ inference_test.py
@@ -12,9 +12,6 @@ from transformers import (
 
 tokenizer = GPT2Tokenizer.from_pretrained(
   'gpt2',
-#  bos_token = '<|startoftext|>',
-#  eos_token = '<|endoftext|>',
-#  pad_token = '<|pad|>',
 )
 
 model = GPT2Model.from_pretrained('./base_model')
@@ -26,11 +23,8 @@ test_strings = [
 ]
 
 for text in test_strings:
-  #print("Encode\n")
   x = tokenizer.encode(text, return_tensors = 'pt')
-  #print("Generate\n")
   y = model.generate(x, max_length=50, pad_token = '<|pad|>')
-  #print("Decode\n")
   for beam in y:
     beamd = tokenizer.decode(beam, skip_special_tokens = True, max_new_tokens = 50)
     print(beamd)
