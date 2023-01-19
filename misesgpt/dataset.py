a3df25a0b44d58bd938afa807135249899470ca8
David Lanzendörfer <leviathan@libresilicon.com>
2023-01-19 10:18:43 +0000

    Finally!
    The progress bar is working!!!

--- misesgpt/dataset.py
+++ misesgpt/dataset.py
@@ -1,4 +1,5 @@
-import multiprocessing
+from tqdm.auto import tqdm
+from concurrent.futures import ThreadPoolExecutor, as_completed
 
 from misesgpt.html_fetcher import MisesHTMLBookCatalog
 from misesgpt.epub_fetcher import MisesEPUBookCatalog
@@ -11,20 +12,31 @@ class MisesDataset:
     self.sentences = []
     self.max_length = max_length
     self.cache_tokenization = cache_tokenization
-    
-    self.get_paragraphs(MisesHTMLBookCatalog().books_json)
-    self.get_paragraphs(MisesEPUBookCatalog().books_json)
+
+    self.html_books = MisesHTMLBookCatalog()
+    self.epub_books = MisesEPUBookCatalog()
+    self.get_paragraphs()
 
   def process_paragraph(self, p):
     pen = self.tokenizer(p, add_special_tokens=True, truncation=True, max_length=self.max_length, return_special_tokens_mask=True)
     if len(pen) < self.max_length:
       self.sentences.append(p)
+    return True
 
-  def get_paragraphs(self, books):
-    for book in books:
-      with multiprocessing.Pool() as pool:
-        pool.map(self.process_paragraph, books[book])
-        pool.close()
+  def get_paragraphs(self):
+    print("Reading all the JSON files")
+    paragraphs = []
+    for book in self.html_books.books_json:
+      paragraphs += self.html_books.books_json[book]
+    for book in self.epub_books.books_json:
+      paragraphs += self.epub_books.books_json[book]
+
+    with tqdm(total=len(paragraphs)) as pbar:
+      with ThreadPoolExecutor() as ex:
+        futures = [ex.submit(self.process_paragraph, p) for p in paragraphs]
+        for future in as_completed(futures):
+          result = future.result()
+          pbar.update(1)
 
   def __getitem__(self, item):
     if not self.cache_tokenization:
