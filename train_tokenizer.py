ade870fde56ed1eaff99827d6b8db7241951a9aa
David Lanzendörfer <leviathan@libresilicon.com>
2023-02-04 02:56:32 +0000

    Fixing tokenizer training script

--- train_tokenizer.py
+++ train_tokenizer.py
@@ -1,7 +1,11 @@
 from transformers import ReformerTokenizer, ReformerTokenizerFast
 
+import sentencepiece as sp
+
 from misesgpt.html_fetcher import MisesHTMLBookCatalog
 from misesgpt.epub_fetcher import MisesEPUBookCatalog
+
+datafile = 'data.txt'
 
 html_books = MisesHTMLBookCatalog()
 epub_books = MisesEPUBookCatalog()
@@ -11,22 +15,23 @@ for book in html_books.books_json:
 for book in epub_books.books_json:
   paragraphs += epub_books.books_json[book]
 
-for i, p in enumerate(paragraphs):
-  paragraphs[i] = '<|startoftext|>' + p + '<|endoftext|>'
+with open(datafile,'w') as f:
+  for p in paragraphs:
+    f.write('<s>' + p + '</s>\n')
+  f.close()
+
+sp.SentencePieceTrainer.train(input=datafile,model_prefix='model/spiece', vocab_size=52_000)
 
 # Initialize a tokenizer
-tokenizer = ReformerTokenizerFast(
-  vocab_file = 'model/spiece.model',
+tokenizer = ReformerTokenizer.from_pretrained(
+  'model',
   return_special_tokens_mask = True,
-  bos_token = '<|startoftext|>',
-  eos_token = '<|endoftext|>',
-  pad_token = '<|pad|>',
-  unk_token = '<|unk|>',
-  mask_token = '<|mask|>',
+  bos_token = '<s>',
+  eos_token = '</s>',
+  pad_token = '<pad>',
+  unk_token = '<unk>',
+  mask_token = '<mask>',
   padding = True,
 )
 
-#help(tokenizer)
-new_tokenizer = tokenizer.train_new_from_iterator(paragraphs, 52_000)
-# Save files to disk
-new_tokenizer.save_vocabulary('model')
+tokenizer.save_pretrained('model')
