b095fbedac2d32a13bc248656f692dabefedfdfa
David Lanzendörfer <leviathan@libresilicon.com>
2023-02-05 15:18:38 +0000

    Tokenize during training
    There's not remotely enough RAM available if we wanna try to pre-tokenize
    it, so we've got to just sort the datasets and tokenize the fragments during
    training.
    It should be OK, as long as we use the fast tokenizer.

--- misesgpt/basic_words.py
+++ misesgpt/basic_words.py
@@ -10,16 +10,16 @@ class MisesBasicWords:
   words = {}
   
   def __init__(self):
+    # Word,Meaning,Examples/0,Examples/1,Examples/2,Examples/3,Examples/4,Examples/5,Examples/6,Examples/7,Examples/8,Examples/9
     with open('word-meaning-examples.csv') as f:
-      c = csv.reader(f) #, delimiter=',', quotechar='|')
+      c = csv.reader(f)
       for l in c:
         if len(l)>1:
           self.words[l[0]]=[]
         if len(l)>2:
           self.words[l[0]].append(l[0]+': '+l[1])
-        for i in range(2,20):
+        for i in range(2,12):
           if len(l)>i+1:
             if len(l[i])>0:
               self.words[l[0]].append(l[i])
       f.close()
-
--- misesgpt/dataset.py
+++ misesgpt/dataset.py
@@ -21,16 +21,15 @@ from torch.utils.data import Dataset
 
 class MisesDataset(Dataset):
   cache_folder = 'mises_dataset_cache'
-  book_sentences = None
-  book_items = None
+  items = []
+  book_frags = None
 
   def __init__(self, tokenizer, max_length, only_build_cache=False, cached_only=False):
     assert(tokenizer is not None)
     self.only_build_cache = only_build_cache
     self.tokenizer = tokenizer
     self.max_length = max_length
-    self.items = []
-    
+
     if not exists(self.cache_folder):
       mkdir(self.cache_folder)
 
@@ -60,16 +59,19 @@ class MisesDataset(Dataset):
       f.close()
 
   def process_paragraph(self, p):
-    s = '<s>' + p + '</s>\n'
-    new_tokens = self.tokenizer.encode(s, add_special_tokens=True, truncation=True, max_length=self.max_length, return_special_tokens_mask=True)
-    if len(new_tokens)+self.book_fragments['length'] <= self.max_length:
-      self.book_fragments['text'] += s
-      self.book_fragments['length'] += len(new_tokens)
-    else:
-      text_tokens = self.tokenizer.encode(self.book_fragments['text'], add_special_tokens=True, truncation=True, max_length=self.max_length, return_special_tokens_mask=True)
-      self.book_fragments['text'] += '<pad>'*(self.max_length-len(text_tokens))
-      pen = self.tokenizer(self.book_fragments['text'], add_special_tokens=True, truncation=True, max_length=self.max_length, return_special_tokens_mask=True)
-      self.book_items.append(pen)
+    s = '<s>' + p + '</s>'
+    new_tokens = self.tokenizer.encode(s, add_special_tokens=True, return_special_tokens_mask=True)
+    if len(new_tokens)+self.book_fragment['length'] > self.max_length:
+      # add padding
+      padding = (self.max_length-self.book_fragment['length'])*'<pad>'
+      self.book_fragment['text'] += padding
+      self.book_frags.append(self.book_fragment['text'])
+      # reset
+      self.book_fragment['text'] = ""
+      self.book_fragment['length'] = 0
+
+    self.book_fragment['text'] += s
+    self.book_fragment['length'] += len(new_tokens)
 
     return True
 
@@ -81,13 +83,14 @@ class MisesDataset(Dataset):
           self.items += pickle.load(f)
           f.close()
       return True
+
     words = []
     for word in word_dict:
       words += word_dict[word]
 
     print("Processing words...")
-    self.book_fragments = { 'length':0, 'text':""}
-    self.book_items = []
+    self.book_fragment = { 'length':0, 'text':""}
+    self.book_frags = []
     with tqdm(total=len(words)) as pbar:
       with ThreadPoolExecutor() as ex:
         futures = [ex.submit(self.process_paragraph, p) for p in words]
@@ -96,10 +99,10 @@ class MisesDataset(Dataset):
           pbar.update(1)
 
     if not self.only_build_cache:
-      self.items += self.book_items
+      self.items += self.book_frags
 
     with open(pickle_file,'wb') as f:
-      pickle.dump( self.book_items, f)
+      pickle.dump( self.book_frags, f)
       f.close()
 
   def get_paragraphs(self, books_json):
@@ -114,8 +117,8 @@ class MisesDataset(Dataset):
         continue
 
       print("Processing entry", book, '(',i,'of',len(books_json),')')
-      self.book_fragments = { 'length':0, 'text':""}
-      self.book_items = []
+      self.book_fragment = { 'length':0, 'text':""}
+      self.book_frags = []
       with tqdm(total=len(books_json[book])) as pbar:
         with ThreadPoolExecutor() as ex:
           futures = [ex.submit(self.process_paragraph, p) for p in books_json[book]]
@@ -124,13 +127,17 @@ class MisesDataset(Dataset):
             pbar.update(1)
 
       if not self.only_build_cache:
-        self.items += self.book_items
+        self.items += self.book_frags
       with open(pickle_file,'wb') as f:
-        pickle.dump( self.book_items, f)
+        pickle.dump( self.book_frags, f)
         f.close()
 
   def __getitem__(self, item):
-    return self.items[item]
+    return self.tokenizer(
+      self.items[item],
+      add_special_tokens=True,
+      return_special_tokens_mask=True
+    )
 
   def __len__(self):
     return len(self.items)
