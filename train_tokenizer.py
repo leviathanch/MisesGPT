d5d15e28e267fd4947c83ee9b0d20db419cfcebb
David Lanzendörfer <leviathan@libresilicon.com>
2023-02-10 17:27:02 +0000

    No more hugging face
    Directly using trax

--- train_tokenizer.py
+++ train_tokenizer.py
@@ -1,5 +1,3 @@
-from transformers import ReformerTokenizer, ReformerTokenizerFast
-
 import sentencepiece as sp
 
 from misesgpt.html_fetcher import MisesHTMLBookCatalog
@@ -25,18 +23,4 @@ with open(datafile,'w') as f:
     f.write('<s>' + p + '</s>\n')
   f.close()
 
-sp.SentencePieceTrainer.train(input=datafile,model_prefix='model/spiece', vocab_size=52_000)
-
-# Initialize a tokenizer
-tokenizer = ReformerTokenizer.from_pretrained(
-  'model',
-  return_special_tokens_mask = True,
-  bos_token = '<s>',
-  eos_token = '</s>',
-  pad_token = '<pad>',
-  unk_token = '<unk>',
-  mask_token = '<mask>',
-  padding = True,
-)
-
-tokenizer.save_pretrained('model')
+sp.SentencePieceTrainer.train(input=datafile,model_prefix='model/remilamda',vocab_size=52_000)
