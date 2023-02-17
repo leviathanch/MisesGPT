95f0696b4fd8a6e2397cd35c535d1c329073c856
David Lanzendörfer <leviathan@libresilicon.com>
2023-02-17 02:20:16 +0000

    Introducing distributed mode
    I need that for pooling

--- train_model.py
+++ train_model.py
@@ -11,9 +11,11 @@ from os.path import exists
 from os import mkdir
 from os import remove
 from os.path import join
+import os
 from math import ceil, log
 import argparse
 import datetime
+import json
 
 from transformers import (
   ReformerConfig,
@@ -22,6 +24,7 @@ from transformers import (
   DataCollatorForLanguageModeling,
   Trainer,
   TrainingArguments,
+  set_seed
 )
 from transformers.optimization import get_linear_schedule_with_warmup
 
@@ -41,163 +44,130 @@ import tensorflow as tf
 
 from misesgpt.dataset import MisesDataset
 
-current_time = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
-train_log_dir = 'runs/' + current_time
-train_summary_writer = tf.summary.create_file_writer(train_log_dir)
+max_length = 32*64
 
-max_length = 64*64
-
-tokenizer = ReformerTokenizerFast(
-  vocab_file = 'model/remilamda.model',
-  return_special_tokens_mask = True,
-  add_special_tokens=True,
-  padding = True,
-  bos_token = '<s>',
-  eos_token = '</s>',
-  pad_token = '<pad>',
-  unk_token = '<unk>',
-  mask_token = '<mask>',
-)
-
-def get_data(args):
-  dataset = MisesDataset(tokenizer, max_length, cached_only=True)
-  data_collator = DataCollatorForLanguageModeling(
-    tokenizer = tokenizer,
-    mlm = True,
-    pad_to_multiple_of = 64*64,
-    return_tensors = 'pt'
+def get_tokenizer():
+  tokenizer = ReformerTokenizerFast.from_pretrained(
+    'remitokenizer',
+    return_special_tokens_mask = True,
+    add_special_tokens = True,
+    max_length = max_length,
+    padding = 'max_length',
   )
-  loader = DataLoader(dataset, collate_fn=data_collator, batch_size=args.batch_size)
-  return loader
+  return tokenizer
 
-def get_model():
+def get_model(tokenizer):
   if exists('./model/config.json'):
-    print("Found existing model. Loading...")
-    model = ReformerModelWithLMHead.from_pretrained('./model')
-    model.eval()
-    warmup = 0
+    print("Found model config...")
+    if exists('./model/pytorch_model.bin'):
+        print("Found pretrained model...")
+        model = ReformerModelWithLMHead.from_pretrained('./model')
+        warmup = 0
+    else:
+      with open('./model/config.json', 'r') as f:
+        cfg = json.load(f)
+        f.close()
+      config = ReformerConfig(**cfg)
+      model = ReformerModelWithLMHead(config)
+      model.resize_token_embeddings(len(tokenizer))
   else:
-    cfg = {
-      "attention_head_size": 64,
-      "attention_probs_dropout_prob": 0.1,
-      "attn_layers": [
-        "local",
-        "lsh",
-        "local",
-        "lsh",
-        "local",
-        "lsh"
-      ],
-    "sinusoidal_pos_embds": True,
-      "axial_norm_std": 1.0,
-      "axial_pos_embds": True,
-      "axial_pos_embds_dim": [32, 96],
-      "axial_pos_shape": [64, 64],
-      "chunk_size_feed_forward": 0,
-      "chunk_size_lm_head": 0,
-      "eos_token_id": 2,
-      "feed_forward_size": 512,
-      "hidden_act": "relu",
-      "hidden_dropout_prob": 0.05,
-      "hidden_size": 128,
-      "initializer_range": 0.02,
-      "intermediate_size": 3072,
-      "is_decoder": True,
-      "layer_norm_eps": 1e-12,
-      "local_attn_chunk_length": 64,
-      "lsh_attn_chunk_length": 64,
-      "max_position_embeddings": 524288,
-      "model_type": "reformer",
-      "num_attention_heads": 2,
-      "num_buckets": [64, 128],
-      "num_chunks_after": 0,
-      "num_chunks_before": 1,
-      "num_hashes": 1,
-      "num_hidden_layers": 6,
-      "output_past": True,
-      "pad_token_id": 0,
-      "task_specific_params": {
-        "text-generation": {
-          "do_sample": True,
-          "max_length": 100
-        }
-      },
-      "vocab_size": 52000
-    }
-    config = ReformerConfig(**cfg)
-    model = ReformerModelWithLMHead(config)
-    model.resize_token_embeddings(len(tokenizer))
+    raise Exception("No model found")
+
   return model
 
-def train_cuda(gpu, args):
-  train_loader = args.train_loader
-  r = args.nr*args.cores+gpu
-  dist.init_process_group(
-    'nccl',
-    rank=r,
-    world_size=args.world_size
+def compute_metrics(pred):
+  non_padded_indices = (pred.label_ids != -100)
+  # correctly shift labels and pred as it's done in forward()
+  labels = pred.label_ids[..., 1:][non_padded_indices[..., 1:]]
+  pred = np.argmax(pred.predictions[:, :-1], axis=-1)[non_padded_indices[..., :-1]]
+  acc = np.mean(np.asarray(pred == labels), dtype=np.float)
+  return {"accuracy": acc}
+
+def train_cuda(args):
+  os.environ["WANDB_DISABLED"] = "true"
+  os.environ["WORLD_SIZE"] = str(args.world_size)
+  os.environ["MASTER_ADDR"] = str(args.master_address)
+  os.environ["MASTER_PORT"] = str(args.master_port)
+  gpu = args.use_core
+
+  training_args = {
+      "max_steps": args.max_steps,
+      "do_train": True,
+      "evaluation_strategy": 'steps',
+      "logging_steps": args.log_steps,
+      "warmup_steps": args.warmup_steps,
+      "per_device_train_batch_size": args.batch_size,
+      "save_steps": args.save_steps,
+      "output_dir": args.output_dir,
+  }
+  if args.weight_decay is not None:
+    training_args["weight_decay"] = args.weight_decay
+  if args.learning_rate is not None:
+    training_args["learning_rate"] = args.learning_rate
+  if args.gradient_accumulation_steps is not None:
+    training_args["gradient_accumulation_steps"] = args.gradient_accumulation_steps
+
+  print(args.distributed)
+  if args.distributed:
+    set_device(gpu)
+    training_args["local_rank"] = args.nr*args.cores+gpu
+    os.environ["RANK"] = str(args.nr*args.cores+gpu)
+
+  train_size = int(0.9 * len(args.dataset))
+  train_dataset, val_dataset = random_split(args.dataset, [train_size, len(args.dataset) - train_size])
+
+  training_args = TrainingArguments(**training_args)
+  trainer = Trainer(
+    model = DistributedDataParallel(args.model.cuda(), device_ids=[gpu]) if args.distributed else args.model,
+    args = training_args,
+    #compute_metrics = compute_metrics,
+    data_collator = args.data_collator,
+    #train_dataset = train_dataset,
+	train_dataset = args.dataset,
+	#eval_dataset = args.dataset,
+    eval_dataset = val_dataset,
   )
-  set_device(gpu)
-  model = DistributedDataParallel(args.model.cuda(), device_ids=[gpu])
-  model = model.train().to('cuda')
-
-  total_step = len(train_loader)
-  optimizer = AdamW(model.module.reformer.parameters())
-  scheduler =  get_linear_schedule_with_warmup(
-    optimizer,
-    num_warmup_steps=args.warmup_steps,
-    num_training_steps=total_step
-  )
-  accelerator = Accelerator(gradient_accumulation_steps=args.gradient_accumulation_steps)
-  
-  for epoch in range(args.epochs):
-    for i, item in enumerate(train_loader):
-      with accelerator.accumulate(model):
-        x = item.to('cuda')
-        loss = model(**x).loss
-        loss.backward()
-        optimizer.step()
-        scheduler.step()
-        optimizer.zero_grad()
-
-        if (i + 1) % args.log_steps == 0 and gpu == 0:
-          print('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}'.format(
-            epoch + 1, 
-            args.epochs,
-            i + 1, 
-            total_step,
-            loss.item())
-          )
-          with train_summary_writer.as_default():
-            tf.summary.scalar('loss', loss.item(), step=i+1)
-
-        if (i + 1) % args.save_steps == 0 and gpu == 0:
-          output_dir = join(args.output_dir, 'checkpoint-{}'.format(epoch*total_step+i+1))
-          print('Savin checkpoint',output_dir)
-          model.module.save_pretrained(output_dir)
+  trainer.train()
 
 def main():
   parser = argparse.ArgumentParser()
-  parser.add_argument('--use-cuda', default=True, type=bool, help='Whether to use CUDA')
-  parser.add_argument('--use-tpus', default=True, type=bool, help='Whether to use TPUs')
+  parser.add_argument('--distributed', action='store_true', help='Turn on distributed mode')
   parser.add_argument('-bs', '--batch-size', default=1, type=int, help='size of a batch')
   parser.add_argument('-n', '--nodes', default=1, type=int, metavar='N')
   parser.add_argument('-c', '--cores', default=1, type=int, help='number of cores per node')
   parser.add_argument('-nr', '--nr', default=0, type=int, help='ranking within the nodes')
   parser.add_argument('--output-dir', default='./out', type=str, help='Output directory')
-  parser.add_argument('--epochs', default=2, type=int, metavar='N', help='number of total epochs to run')
+  parser.add_argument('--max-steps', default=2000, type=int, metavar='N', help='Maximums steps')
   parser.add_argument('--save-steps', default=100, type=int, help='Save every N steps')
   parser.add_argument('--warmup-steps', default=50, type=int, help='Warmup steps')
   parser.add_argument('--log-steps', default=100, type=int, help='Log every N steps')
+  parser.add_argument('--gradient-accumulation-steps', type=int, help='Gradient accumulation steps')
+  parser.add_argument('--use-core', default=0, type=int, help='The core to spin up on')
+  parser.add_argument('--master-address', default="127.0.0.1", type=str, help='The master address')
+  parser.add_argument('--master-port', default="8888", type=str, help='The master port')
+  parser.add_argument('--learning-rate', default=1e-3, type=float, help='Learning rate')
+  parser.add_argument('--weight-decay', type=float, help='Weight decay')
+  parser.add_argument('--seed', type=int, help='Seed')
+
+  #config = tf.compat.v1.ConfigProto()
+  #config.gpu_options.allow_growth = True
+  #sess = tf.compat.v1.Session(config=config)
+
   args = parser.parse_args()
   args.world_size = args.cores*args.nodes
-  args.train_loader = get_data(args)
-  args.model = get_model()
-
-  if args.use_cuda:
-    mp.spawn(train_cuda, nprocs=args.cores, args=(args,))
-  elif args.use_tpus: # TODO: support TPUs
-    pass
+  tokenizer = get_tokenizer()
+  if args.seed is not None:
+    set_seed(args.seed)
+  args.model = get_model(tokenizer)
+  args.dataset = MisesDataset(tokenizer, max_length, 64, cached_only=True)
+  args.data_collator = DataCollatorForLanguageModeling(
+    tokenizer = tokenizer,
+    #mlm = False,
+    mlm = True,
+    mlm_probability = 0.15,
+    return_tensors = 'pt',
+  )
+  train_cuda(args)
 
 if __name__ == '__main__':
   main()
