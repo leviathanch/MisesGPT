b7baaccec2f294369159cb6f0e15d613e123a07f
David Lanzendörfer <leviathan@libresilicon.com>
2023-02-17 02:17:57 +0000

    Fixing the training of the tokenizer
    Now it finally properly processes <s>

--- train_tokenizer.py
+++ train_tokenizer.py
@@ -4,6 +4,9 @@ from misesgpt.html_fetcher import MisesHTMLBookCatalog
 from misesgpt.epub_fetcher import MisesEPUBookCatalog
 from misesgpt.basic_words import MisesBasicWords
 
+from transformers import  ReformerTokenizerFast
+from tokenizers import AddedToken
+
 datafile = 'data.txt'
 
 word_definitions = MisesBasicWords()
@@ -20,20 +23,36 @@ for book in epub_books.books_json:
 
 with open(datafile,'w') as f:
   for p in paragraphs:
-    f.write('<s>' + p + '</s>\n')
+    f.write(p+'\n')
   f.close()
 
-uds = '<mask>'
-for i in range(10):
-  uds += ',<speaker'+str(i)+'>'
+#uds = '<mask>'
+#for i in range(10):
+#  uds += ',<speaker'+str(i)+'>'
 
 sp.SentencePieceTrainer.train(
   input=datafile,
   model_prefix='model/remilamda',
   vocab_size=52_000,
-  bos_id=0,
-  pad_id=1,
-  eos_id=2,
-  unk_id=3,
-  user_defined_symbols=uds,
+  #user_defined_symbols=uds,
+  max_sentence_length=8192,
 )
+
+tokenizer = ReformerTokenizerFast(
+  vocab_file = 'model/remilamda.model',
+  return_special_tokens_mask = True,
+  add_special_tokens = True,
+  padding = True,
+  bos_token = AddedToken('<s>'),
+  eos_token = AddedToken('</s>'),
+  pad_token = AddedToken('<pad>'),
+  unk_token = AddedToken('<unk>'),
+  mask_token = AddedToken('<mask>'),
+  sep_token = AddedToken('<sep>'),
+)
+tokenizer.add_special_tokens({"additional_special_tokens": [
+  AddedToken("\n")
+]})
+
+tokenizer.train_new_from_iterator(paragraphs, 52000)
+tokenizer.save_pretrained("remitokenizer")
